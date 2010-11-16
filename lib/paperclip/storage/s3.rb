af18aeed3670718bd1044709adfa5a4721226a93
jhe <judy.w.he@gmail.com>
2010-11-16 14:03:13 -0500

    go back to getting s3 to set content type

--- lib/paperclip/storage/s3.rb
+++ lib/paperclip/storage/s3.rb
@@ -149,9 +149,7 @@ module Paperclip
             AWS::S3::S3Object.store(style_path,
                                     file,
                                     bucket_name,
-                                    {:content_type => instance_read(:content_type),
-                                     :access => @s3_permissions,
-                                    }.merge(@s3_headers))
+                                    {:access => @s3_permissions}.merge(@s3_headers))
           rescue AWS::S3::NoSuchBucket => e
             create_bucket
             retry
